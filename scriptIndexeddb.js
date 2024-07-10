771f9b7d10e995f29c74ad98d28d20dbf88b2018
GuestFromSpaceX <gfromsx@gmail.com>
2024-07-10 20:51:37 +0300

    feat: mvp done

--- scriptIndexeddb.js
+++ scriptIndexeddb.js
@@ -28,7 +28,6 @@ request.onsuccess = function () {
     
     const mainBlock = document.getElementById('block');
     const mainSearch = document.getElementById('mainSearch');
-    const displayHandler = true;
     
     displayTasks(db, 'existance', 1);
 
@@ -112,6 +111,7 @@ function DOMElement(db, task, mainBlock) {
 
     const archiveButton = document.createElement('button');
     archiveButton.classList.add('main__arch');
+    archiveButton.id = 'archiveButtonLarge';
     archiveButton.textContent = 'Архивировать';
     
     taskDiv.appendChild(archiveButton);
@@ -119,6 +119,7 @@ function DOMElement(db, task, mainBlock) {
     const archiveButtonHidden = document.createElement('button');
     archiveButtonHidden.classList.add('main__arch--hidden');
     archiveButtonHidden.display = 'none';
+    archiveButtonHidden.id = 'archiveButtonSmall';
     archiveButtonHidden.innerHTML = '<img id="archive" src="./media/archive.svg"  >';
 
     taskDiv.appendChild(archiveButtonHidden);
@@ -132,6 +133,23 @@ function DOMElement(db, task, mainBlock) {
     inputText.addEventListener('change', function () {
         updateText(db, task, inputText);
     })
+
+    archiveButton.addEventListener('click', function() {
+        archiveTask(db, task);
+    })
+
+    archiveButtonHidden.addEventListener('click', function() {
+        archiveTask(db, task);
+    })
+}
+
+function archiveTask(db, task) {
+    const transaction = db.transaction("tasks", "readwrite");
+    const store = transaction.objectStore("tasks");
+
+    store.delete(task.id);
+
+    window.location.reload();
 }
 
 function addTask(db) {
@@ -139,15 +157,9 @@ function addTask(db) {
     const store = transaction.objectStore("tasks");
     const newTask = { checked: false, text: '', exist: 1 };
 
-    const request = store.add(newTask);
-    // request.onsuccess = function () {
-    //     displayTasks(db);
-    // }
+    store.add(newTask);
+    
     window.location.reload();
-
-    request.onerror = function (event) {
-        console.error("Error adding task:", event);
-    }
 }
 
 function checkTask(db, task, inputText) {
